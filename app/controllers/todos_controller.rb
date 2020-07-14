fcbc2547a1a92faf07f9bcd1d8cda2da3b2de71b
mateen47 <mateen.ismail17@gmail.com>
2020-07-14 07:11:51 -0400

    add edit

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -15,6 +15,21 @@ class TodosController<ApplicationController
 	def show
 		@todo = Todo.find(params[:id])
 	end
+	def edit
+		@todo = Todo.find(params[:id])
+	end
+	def update
+		@todo = Todo.find(params[:id])
+
+		if @todo.update(todo_params)
+			flash[:notice]="Todo was successfully updated"
+			redirect_to todo_path(@todo)
+		else
+			flash[:notice]="Todo was not updated"
+			render 'edit'
+		end
+	end
+
 	private
 	def todo_params
 		params.require(:todo).permit(:name , :description)
